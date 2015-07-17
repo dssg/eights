6e73ba9096102c46cfd790fe585196fce304ad4e
zar1 <zachary.rubenstein@gmail.com>
2015-07-17 12:25:10 -0500

    wrote feature_pairs_in_tree

--- tests/test_operate.py
+++ tests/test_operate.py
@@ -5,3 +5,26 @@
 #tst_w = 1
 #ret = expanding_window(l,w,tst_w)
 #for x in ret: print x 
+
+import unittest
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+
+import utils_for_tests as utils
+utils.add_to_python_path()
+
+from eights.operate.operate_helper import feature_pairs_in_tree
+
+class TestOperate(unittest.TestCase):
+    def test_feature_pairs_in_tree(self):
+        iris = datasets.load_iris()
+        rf = RandomForestClassifier(random_state=0)
+        rf.fit(iris.data, iris.target)
+        dt = rf.estimators_[0]
+        result = feature_pairs_in_tree(dt)
+        ctrl = [[(3, 2)], [(2, 3), (2, 0)], [(0, 2), (3, 1)]]
+        self.assertEqual(result, ctrl)
+
+if __name__ == '__main__':
+    unittest.main()
