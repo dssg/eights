901ac57f3562f6e95bcb4350c52715ba1e662b53
John Santerre <john@johnsanterre.com>
2015-07-27 11:14:56 -0500

    operate

--- eights/operate/operate.py
+++ eights/operate/operate.py
@@ -2,13 +2,62 @@ from operate_helper import *
 import eights.investigate as inv
 import eights.utils as utils
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.dummy import DummyClassifier
+from sklearn.cross_validation import StratifiedKFold
+
+
+
 # operate is full of liiitle pipelines
 
-def run_classifier(M, y, clf_class, hyperparameters):
-    # returns fitted estimator
-    raise NotImplementedError
+def run_classifier(M, labels, clfs):
+    """function for running a single classifier
+    Parameters
+    ----------
+    M : structured array
+    Matrix  
+    
+    labels : np.ndarray
+    the correct labels
 
-def run_std_classifiers(M_train, M_test, y_train, y_test, report_file):
+    Returns
+    -------
+    exp : Experiment
+    Description
+
+    Example
+    -------
+
+    """
+    exp = Experiment(
+        M, 
+        labels, 
+        clfs=clfs)
+    return exp
+
+def run_std_classifiers(M, labels, clfs=None, cvs=None, report_file='report.pdf'):
+    if clfs is None:
+        clfs = {AdaBoostClassifier: {'n_estimators': [20,50,100]}, 
+               RandomForestClassifier: {'n_estimators': [10,30,50],'max_depth': [None,4,7,15],'n_jobs':[1]}, 
+               LogisticRegression:{'C': [1.0,2.0,0.5,0.25],'penalty': ['l1','l2']}, 
+               DecisionTreeClassifier {'max_depth': [None,4,7,15,25]},
+               SVC:{'kernel': ['linear','rbf']}
+               DummyClassifier:{'strategy': ['stratified','most_frequent','uniform']}
+              }        
+    if cv == None:
+        cv = {StratifiedKFold:{}}
+    
+    exp = Experiment(
+        M, 
+        labels, 
+        clfs=clfs,
+        cvs = cvs
+        )
+    
     raise NotImplementedError    
 
 def load_and_run_rf_cv(file_loc, label_col=0):
