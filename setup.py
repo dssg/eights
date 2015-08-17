c8f0ef36a410bd2cf9cbff56d64b8495ce537418
zar1 <zachary.rubenstein@gmail.com>
2015-08-17 15:27:52 -0500

    syntax error

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
         name='eights',
         version='0.0.1',
         url='https://github.com/dssg/eights',
-        author='Center for Data Science and Public Policy'
-        description='A library and workflow template for machine learning'
+        author='Center for Data Science and Public Policy',
+        description='A library and workflow template for machine learning',
         packages=find_packages(),
         install_requires=('numpy',
                           'scikit-learn', 
