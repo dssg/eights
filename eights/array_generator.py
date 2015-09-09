830462bd832bee28c394c174d18c36afca82a700
zar1 <zachary.rubenstein@gmail.com>
2015-09-09 12:13:01 -0500

    finished second pass at documentation

--- eights/array_generator.py
+++ eights/array_generator.py
@@ -1,5 +1,4 @@
-import numpy as np
-
+import numpy as np 
 class ArrayGenerator(object):
     """
     Array generator is a tool to confederate tables from either SQL or CSV,
@@ -20,15 +19,15 @@ class ArrayGenerator(object):
 
         *Table 1:*
 
-        +------------+-----------+----------------+
-        | student_id | grad_year |        address |
-        +============+===========+================+
-        |          0 |      2007 | 1234 N Halsted |
-        +------------+-----------+----------------+
-        |          1 |      2008 | 5555 W Addison |
-        +------------+-----------+----------------+
-        |          2 |      2007 |   2726 N Clark |
-        +------------+-----------+----------------+
+            +------------+-----------+----------------+
+            | student_id | grad_year |        address |
+            +============+===========+================+
+            |          0 |      2007 | 1234 N Halsted |
+            +------------+-----------+----------------+
+            |          1 |      2008 | 5555 W Addison |
+            +------------+-----------+----------------+
+            |          2 |      2007 |   2726 N Clark |
+            +------------+-----------+----------------+
 
         In Table 1, each unit is a student. Each student has his or her own 
         row, which is uniquely identified by its entry in the student_id 
@@ -45,11 +44,11 @@ class ArrayGenerator(object):
 
         In a log format table, there are four columns:
         
-        1. The unique identifier of a unit. Unit here means the same thing
-           as it did static format tables.
-        2. A time frame for which the column is applicable.
-        3. The name of a feature applicable to that unit at that time.
-        4. The value of the feature for that unit at that time. 
+            1. The unique identifier of a unit. Unit here means the same thing
+               as it did static format tables.
+            2. A time frame for which the column is applicable.
+            3. The name of a feature applicable to that unit at that time.
+            4. The value of the feature for that unit at that time. 
 
         The values in column one still uniquely identify each unit, but there
         can be more than one row in the table per unit. These tables give us
@@ -60,37 +59,37 @@ class ArrayGenerator(object):
 
         *Table 2:*
 
-        +------------+------+-------------+-------+
-        | student_id | year |     feature | value |
-        +============+======+=============+=======+
-        |          0 | 2005 |    math_gpa |   2.3 |
-        +------------+------+-------------+-------+
-        |          0 | 2005 | english_gpa |   4.0 |
-        +------------+------+-------------+-------+
-        |          0 | 2005 |    absences |     7 |
-        +------------+------+-------------+-------+
-        |          0 | 2006 |    math_gpa |   2.1 |
-        +------------+------+-------------+-------+
-        |          0 | 2006 | english_gpa |   3.9 |
-        +------------+------+-------------+-------+
-        |          0 | 2006 |    absences |     8 |
-        +------------+------+-------------+-------+
-        |          1 | 2005 |    math_gpa |   3.4 |
-        +------------+------+-------------+-------+
-        |          1 | 2005 |    absenses |     0 |
-        +------------+------+-------------+-------+
-        |          1 | 2006 |    math_gpa |   3.5 |
-        +------------+------+-------------+-------+
-        |          1 | 2007 | english_gpa |   2.4 |
-        +------------+------+-------------+-------+
-        |          2 | 2004 |    math_gpa |   2.4 |
-        +------------+------+-------------+-------+
-        |          2 | 2005 |    math_gpa |   3.4 |
-        +------------+------+-------------+-------+
-        |          2 | 2005 |    absenses |    14 |
-        +------------+------+-------------+-------+
-        |          2 | 2006 |    absenses |    96 |
-        +------------+------+-------------+-------+
+            +------------+------+-------------+-------+
+            | student_id | year |     feature | value |
+            +============+======+=============+=======+
+            |          0 | 2005 |    math_gpa |   2.3 |
+            +------------+------+-------------+-------+
+            |          0 | 2005 | english_gpa |   4.0 |
+            +------------+------+-------------+-------+
+            |          0 | 2005 |    absences |     7 |
+            +------------+------+-------------+-------+
+            |          0 | 2006 |    math_gpa |   2.1 |
+            +------------+------+-------------+-------+
+            |          0 | 2006 | english_gpa |   3.9 |
+            +------------+------+-------------+-------+
+            |          0 | 2006 |    absences |     8 |
+            +------------+------+-------------+-------+
+            |          1 | 2005 |    math_gpa |   3.4 |
+            +------------+------+-------------+-------+
+            |          1 | 2005 |    absenses |     0 |
+            +------------+------+-------------+-------+
+            |          1 | 2006 |    math_gpa |   3.5 |
+            +------------+------+-------------+-------+
+            |          1 | 2007 | english_gpa |   2.4 |
+            +------------+------+-------------+-------+
+            |          2 | 2004 |    math_gpa |   2.4 |
+            +------------+------+-------------+-------+
+            |          2 | 2005 |    math_gpa |   3.4 |
+            +------------+------+-------------+-------+
+            |          2 | 2005 |    absenses |    14 |
+            +------------+------+-------------+-------+
+            |          2 | 2006 |    absenses |    96 |
+            +------------+------+-------------+-------+
 
         In Table 2, the values of the student_id column each correspond to
         one student, and, consequently, one row in feature 1. However, each
@@ -107,12 +106,12 @@ class ArrayGenerator(object):
         of the 'feature' column in each log table. For example, if we 
         confederate Table 1 and Table 2, we will have columns for:
 
-        * student_id
-        * grad_year
-        * address
-        * absences
-        * math_pga
-        * english_gpa
+            * student_id
+            * grad_year
+            * address
+            * absences
+            * math_pga
+            * english_gpa
 
         Each row in the resulting structured array will represent one student 
         and will have a unique student_id.
@@ -120,8 +119,8 @@ class ArrayGenerator(object):
         In order to decide what value appears in the columns originating from 
         the log-format table, we:
 
-        1. Optionally select an aggregation method with set_aggregation
-        2. Select a timeframe with to_sa
+            1. Optionally select an aggregation method with set_aggregation
+            2. Select a timeframe with to_sa
 
         When creating the structured array, we first take only entries of log
         tables that fall within the timeframe, then we aggregate those entries
@@ -138,15 +137,15 @@ class ArrayGenerator(object):
         
         *Table 3:*
 
-        +------------+-----------+----------------+----------+-------------+----------+
-        | student_id | grad_year |        address | math_gpa | english_gpa | absences |
-        +============+===========+================+==========+=============+==========+
-        |          0 |      2007 | 1234 N Halsted |      2.2 |        3.95 |        8 |
-        +------------+-----------+----------------+----------+-------------+----------+
-        |          1 |      2008 | 5555 W Addison |     3.45 |         nan |        0 |
-        +------------+-----------+----------------+----------+-------------+----------+
-        |          2 |      2007 |   2726 N Clark |      3.4 |         nan |       94 |
-        +------------+-----------+----------------+----------+-------------+----------+
+            +------------+-----------+----------------+----------+-------------+----------+
+            | student_id | grad_year |        address | math_gpa | english_gpa | absences |
+            +============+===========+================+==========+=============+==========+
+            |          0 |      2007 | 1234 N Halsted |      2.2 |        3.95 |        8 |
+            +------------+-----------+----------------+----------+-------------+----------+
+            |          1 |      2008 | 5555 W Addison |     3.45 |         nan |        0 |
+            +------------+-----------+----------------+----------+-------------+----------+
+            |          2 |      2007 |   2726 N Clark |      3.4 |         nan |       94 |
+            +------------+-----------+----------------+----------+-------------+----------+
 
         Notice that math_gpa and english_gpa are the average for 2005 and 2006
         per student, while absenses is the max over 2005 and 2006. Also notice
@@ -174,13 +173,13 @@ class ArrayGenerator(object):
         
         *Table 4:*
 
-        +------------+-----------+----------------+----------+-------------+----------+
-        | student_id | grad_year |        address | math_gpa | english_gpa | absences |
-        +============+===========+================+==========+=============+==========+
-        |          0 |      2007 | 1234 N Halsted |      2.2 |        3.95 |        8 |
-        +------------+-----------+----------------+----------+-------------+----------+
-        |          2 |      2007 |   2726 N Clark |      3.4 |         nan |       94 |
-        +------------+-----------+----------------+----------+-------------+----------+
+            +------------+-----------+----------------+----------+-------------+----------+
+            | student_id | grad_year |        address | math_gpa | english_gpa | absences |
+            +============+===========+================+==========+=============+==========+
+            |          0 |      2007 | 1234 N Halsted |      2.2 |        3.95 |        8 |
+            +------------+-----------+----------------+----------+-------------+----------+
+            |          2 |      2007 |   2726 N Clark |      3.4 |         nan |       94 |
+            +------------+-----------+----------------+----------+-------------+----------+
 
         
     Parameters
@@ -288,9 +287,44 @@ class ArrayGenerator(object):
             
         """
         pass
+
+    def set_aggregation(self, feature_name, method):
+        """Sets the method used to aggregate across dates in a log table.
+
+        If set_aggregation is not called for a given feature, the method will
+        default to 'mean'
+        
+        Parameters
+        ----------
+        feature_name : str
+            Name of feature for which we are aggregating
+        method : str or list -> float
+            Method used to aggregate the feature across year. If a str, must
+            be one of:
+
+                * 'mean'
+                * 'median'
+                * 'mode'
+                * 'min'
+                * 'max'
+
+            If a function, must take a list and return a float
+
+        Examples
+        --------
+        >>> ag = ArrayGenerator(...)
+        >>> ... # Populate ag with Table 1 and Table 2
+        >>> ag.set_aggregation('math_gpa', 'mean')
+        >>> ag.set_aggregation('absences', 'max')
+        >>> sa = ag.to_sa(2005, 2006)
+        """
+        pass
     
-    def take_subset(self, where):
-        """Specifies a subset of the query to return when calling to_sa
+    def set_constraint(self, where):
+        """
+        
+        Specifies a subset of the units to be returned in the SA according
+        to some constraint.
 
         Parameters
         ----------
@@ -307,10 +341,12 @@ class ArrayGenerator(object):
 
         Examples
         --------
-        >>> tb = ArrayGenerator(...)
-        ...
-        >>> tbsub1 = tb.take_subset('Graduated == 1 and GPA_10 > 2.3')
-        >>> tbsub2 = tb.take_subset('Graduated == 0 and not GPA_9 < 2.0')
+        >>> ag = ArrayGenerator(...)
+        >>> ... # Populate ag with Table 1 and Table 2
+        >>> ag.set_aggregation('math_gpa', 'mean')
+        >>> ag.set_aggregation('absences', 'max')
+        >>> ag = ag.set_constraint('grad_year == 2007')
+        >>> sa = ag.to_sa(2005, 2006)
         """
         # Note that this copies the original rather than mutating it, so
         # taking a subset does not permanently lose data.
@@ -319,8 +355,17 @@ class ArrayGenerator(object):
         # https://github.com/dssg/UPSG/blob/master/upsg/transform/split.py#L210
         return self
         
-    def to_sa(self):
-        """
+    def to_sa(self, start_time, stop_time):
+        """Creates a structured array
+
+        Start times and stop times are inclusive
+
+        Parameters
+        ----------
+        start_time : number or datetime.datetime
+            Start time of log tables to include in this sa
+        stop_time : number or datetime.datetime
+            Stop time of log tables to include in this sa
         Returns
         -------
         np.ndarray
